8c7acdb4460e44c0aaf84c41362f597ca39fc0f2
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2022-08-16 22:40:44 +0200

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -386,7 +386,7 @@ namespace graph{
         }
     
     /****************************************************************************************************/
-
+    
     void dfsts(int a=1) {     // call this function with root node (1) , for preprocessing, then you can use LCA fn
         // tree ancestors dfs
         for(int b : children[a]) {
@@ -455,6 +455,71 @@ namespace graph{
         }
         return node;
     }
+        /***************************************************************************************************************************************/
+            struct LCA {            // another LCA sol using segtree -> build in O(N) & query in O(N.log.N)
+            vector<int> height, euler, first, segtree;
+            vector<bool> visited;
+            int n;
+
+            LCA(vector<vector<int>> &adj, int root = 0) {
+                n = adj.size();
+                height.resize(n);
+                first.resize(n);
+                euler.reserve(n * 2);
+                visited.assign(n, false);
+                dfs(adj, root);
+                int m = euler.size();
+                segtree.resize(m * 4);
+                build(1, 0, m - 1);
+            }
+
+            void dfs(vector<vector<int>> &adj, int node, int h = 0) {
+                visited[node] = true;
+                height[node] = h;
+                first[node] = euler.size();
+                euler.push_back(node);
+                for (auto to : adj[node]) {
+                    if (!visited[to]) {
+                        dfs(adj, to, h + 1);
+                        euler.push_back(node);
+                    }
+                }
+            }
+
+            void build(int node, int b, int e) {
+                if (b == e) {
+                    segtree[node] = euler[b];
+                } else {
+                    int mid = (b + e) / 2;
+                    build(node << 1, b, mid);
+                    build(node << 1 | 1, mid + 1, e);
+                    int l = segtree[node << 1], r = segtree[node << 1 | 1];
+                    segtree[node] = (height[l] < height[r]) ? l : r;
+                }
+            }
+
+            int query(int node, int b, int e, int L, int R) {
+                if (b > R || e < L)
+                    return -1;
+                if (b >= L && e <= R)
+                    return segtree[node];
+                int mid = (b + e) >> 1;
+
+                int left = query(node << 1, b, mid, L, R);
+                int right = query(node << 1 | 1, mid + 1, e, L, R);
+                if (left == -1) return right;
+                if (right == -1) return left;
+                return height[left] < height[right] ? left : right;
+            }
+
+            int lca(int u, int v) {
+                int left = first[u], right = first[v];
+                if (left > right)
+                    swap(left, right);
+                return query(1, 0, euler.size() - 1, left, right);
+            }
+        };
+    /****************************************************************************************************************************************/
     // some graphs generator
     vector<pair<int,int>> csg(int n){
         // construct star graph (also its considered as a tree) -> m-1 edges
@@ -476,6 +541,8 @@ namespace graph{
         }
         return graph;
     }
+
+
     /****************************************************************************************************************************************/
          #define MAXV 50
         #define MAXE 200
