e07ef8fe0c359098bcb1935a5489bcdf9fe0bae5
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2022-08-17 16:54:36 +0200

    I added SCC 2-algo

--- graphs.cpp
+++ graphs.cpp
@@ -682,3 +682,105 @@ namespace graph{
             return ans;
         }
     /****************************************************************************************************************************************/
+        // Kosaraju algo for detecting SCC in O(V+E)
+        // src:: https://www.topcoder.com/thrive/articles/kosarajus-algorithm-for-strongly-connected-components
+        #define MAX_N 20001
+        int n, m;
+        struct Node {
+            vector < int > adj;
+            vector < int > rev_adj;
+        };
+        Node g[MAX_N];
+        stack < int > S;
+        bool visited[MAX_N];
+        int component[MAX_N];
+        vector < int > components[MAX_N];
+        int numComponents;
+        void dfs_1(int x) {
+            visited[x] = true;
+            for (int i = 0; i < g[x].adj.size(); i++) {
+                if (!visited[g[x].adj[i]]) dfs_1(g[x].adj[i]);
+            }
+            S.push(x);
+        }
+        void dfs_2(int x) {
+            printf("%d ", x);
+            component[x] = numComponents;
+            components[numComponents].push_back(x);
+            visited[x] = true;
+            for (int i = 0; i < g[x].rev_adj.size(); i++) {
+                if (!visited[g[x].rev_adj[i]]) dfs_2(g[x].rev_adj[i]);
+            }
+        }
+        void Kosaraju() {
+            for (int i = 0; i < n; i++)
+                if (!visited[i]) dfs_1(i);
+            for (int i = 0; i < n; i++)
+                visited[i] = false;
+            while (!S.empty()) {
+                int v = S.top();
+                S.pop();
+                if (!visited[v]) {
+                    printf("Component %d: ", numComponents);
+                    dfs_2(v);
+                    numComponents++;
+                    printf("\n");
+                }
+            }
+        }
+        int main() {
+            cin >> n >> m;
+            int a, b;
+            while (m--) {
+                cin >> a >> b;
+                g[a].adj.push_back(b);
+                g[b].rev_adj.push_back(a);
+            }
+            Kosaraju();
+            printf("Total number of components: %d\n", numComponents);
+            return 0;
+        }
+    /******************************************************/
+        // Tarjan algo for detecting SCC in O(V+E)
+        // Tarjan algo for detecting SCC in O(V+E)
+        #define MAX 10  // # of vertices
+        int n, m, id, rep[MAX];
+        vector<int> graph[MAX], brdg[MAX];
+        int dfn[MAX], low[MAX], timer;
+        bool in_st[MAX];
+        stack<int> st;
+        void tarjan(int node, int par = 1) {
+        dfn[node] = low[node] = ++timer;
+        in_st[node] = 1;
+        st.push(node);
+        for (auto to : graph[node]) {
+            if (to == par) continue;
+            if (dfn[to] == 0) {
+                tarjan(to, node);
+                low[node] = min(low[node], low[to]);
+            }
+            if (in_st[to])
+                low[node] = min(low[node], dfn[to]);
+        }
+        if (dfn[node] == low[node]) {
+            ++id;
+            while(true) {
+                int cur = st.top();
+                cout << cur << " ";
+                st.pop();
+                in_st[cur] = 0;
+                rep[cur] = id;
+                if (cur == node)
+                    break;
+            }
+            cout <<el;
+        }
+        }
+        void meen(){
+            int a,b;
+            cin >> n >> m;
+            while (m--) cin >> a >> b,graph[a].push_back(b),graph[b].push_back(a);
+            tarjan(1);   // start from any node , changes will be in the order of printing SCC 
+        }
+
+    /****************************************************************************************************************************************/
